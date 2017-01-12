60f8b35a30c9b990c00241dd4f97bfb14d017357
Lovino Maxime <maximelovino@gmail.com>
2017-01-12 15:03:04 +0100

    started LED DMA lab, not working

--- workspace/MyLab_lib/Debug/src/subdir.mk
+++ workspace/MyLab_lib/Debug/src/subdir.mk
@@ -23,7 +23,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -D__REDLIB__ -DDEBUG -D__CODE_RED -DCORE_M3 -D__USE_CMSIS=CMSIS_CORE_LPC17xx -D__USE_CMSIS_DSPLIB=CMSIS_DSPLIB_CM3 -D__LPC17XX__ -I"Z:\PROC\workspace\MyLab_lib\inc" -I"Z:\PROC\workspace\CMSIS_CORE_LPC17xx\inc" -I"Z:\PROC\workspace\MyLab_lib\inc" -O0 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m3 -mthumb -D__REDLIB__ -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	arm-none-eabi-gcc -D__REDLIB__ -DDEBUG -D__CODE_RED -DCORE_M3 -D__USE_CMSIS=CMSIS_CORE_LPC17xx -D__USE_CMSIS_DSPLIB=CMSIS_DSPLIB_CM3 -D__LPC17XX__ -I"/Users/maximelovino/Google Drive/HEPIA/2016-2017/IT_231 - Processeurs/LABS/workspace/MyLab_lib/inc" -I"/Users/maximelovino/Google Drive/HEPIA/2016-2017/IT_231 - Processeurs/LABS/workspace/CMSIS_CORE_LPC17xx/inc" -I"/Users/maximelovino/Google Drive/HEPIA/2016-2017/IT_231 - Processeurs/LABS/workspace/MyLab_lib/inc" -O0 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m3 -mthumb -D__REDLIB__ -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
---
+++ workspace/labo7_led/Debug/src/labo7_led.d
@@ -0,0 +1,24 @@
+src/labo7_led.o src/labo7_led.d: ../src/labo7_led.c \
+ /Users/maximelovino/Google\ Drive/HEPIA/2016-2017/IT_231\ -\ Processeurs/LABS/workspace/CMSIS_CORE_LPC17xx/inc/LPC17xx.h \
+ /Users/maximelovino/Google\ Drive/HEPIA/2016-2017/IT_231\ -\ Processeurs/LABS/workspace/CMSIS_CORE_LPC17xx/inc/core_cm3.h \
+ /Users/maximelovino/Google\ Drive/HEPIA/2016-2017/IT_231\ -\ Processeurs/LABS/workspace/CMSIS_CORE_LPC17xx/inc/core_cmInstr.h \
+ /Users/maximelovino/Google\ Drive/HEPIA/2016-2017/IT_231\ -\ Processeurs/LABS/workspace/CMSIS_CORE_LPC17xx/inc/core_cmFunc.h \
+ /Users/maximelovino/Google\ Drive/HEPIA/2016-2017/IT_231\ -\ Processeurs/LABS/workspace/CMSIS_CORE_LPC17xx/inc/system_LPC17xx.h \
+ ../src/config_LPC1769.h ../src/../../MyLab_lib/inc/GPIO.h \
+ ../src/../../MyLab_lib/inc/config_LPC1769.h
+
+/Users/maximelovino/Google\ Drive/HEPIA/2016-2017/IT_231\ -\ Processeurs/LABS/workspace/CMSIS_CORE_LPC17xx/inc/LPC17xx.h:
+
+/Users/maximelovino/Google\ Drive/HEPIA/2016-2017/IT_231\ -\ Processeurs/LABS/workspace/CMSIS_CORE_LPC17xx/inc/core_cm3.h:
+
+/Users/maximelovino/Google\ Drive/HEPIA/2016-2017/IT_231\ -\ Processeurs/LABS/workspace/CMSIS_CORE_LPC17xx/inc/core_cmInstr.h:
+
+/Users/maximelovino/Google\ Drive/HEPIA/2016-2017/IT_231\ -\ Processeurs/LABS/workspace/CMSIS_CORE_LPC17xx/inc/core_cmFunc.h:
+
+/Users/maximelovino/Google\ Drive/HEPIA/2016-2017/IT_231\ -\ Processeurs/LABS/workspace/CMSIS_CORE_LPC17xx/inc/system_LPC17xx.h:
+
+../src/config_LPC1769.h:
+
+../src/../../MyLab_lib/inc/GPIO.h:
+
+../src/../../MyLab_lib/inc/config_LPC1769.h:
