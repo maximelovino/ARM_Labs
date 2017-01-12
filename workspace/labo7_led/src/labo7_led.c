74c27ab66dc49205cff4d11d125f4f12898665a8
Lovino Maxime <maximelovino@gmail.com>
2017-01-12 15:27:58 +0100

    corrected with teacher, still not working

--- workspace/labo7_led/src/labo7_led.c
+++ workspace/labo7_led/src/labo7_led.c
@@ -32,41 +32,50 @@ void ledSet(uint32_t r, uint32_t g,uint32_t b){
 	//mode compteur
 	T0CTCR = 0;
 	//match register
-	T0MCR = 1;
+	T0MCR = 1<<1;
 	//on match à 3 coups de clock
-	T0MR0 = 3;
+	//period 10ns => 30*10 = 300ns
+	T0MR0 = 30-1;
 	T0TCR = 1; /* start the timer */
+
 	LPC_SC->PCONP = 1<<1;
 	LPC_GPDMA->DMACConfig = 1;
 	LPC_GPDMACH0->DMACCSrcAddr = tabRGB;
-	FIO2MASK = 1 << 8;
-	LPC_GPDMACH0->DMACCDestAddr = FIO2PIN + 8;
-	LPC_GPDMACH0->DMACCControl = (1<<26) + 97;
-	LPC_SC->DMAREQSEL = 1;
-	LPC_GPDMACH0->DMACCConfig = (001<<11) + (T0MR0 << 6) + 1;
 
+	FIO2DIR = 1 << 8;
+	/*Problem here*/
+	FIO2MASK = 1 << 8;
+	//TODO put in define for FIO2PIN1 (8th pin of FIO2PIN)
+	LPC_GPDMACH0->DMACCDestAddr = 0x2009C055;
+
+	//26 source increment
+	LPC_GPDMACH0->DMACCControl = (1<<26) + 97;
+
+	LPC_SC->DMAREQSEL = 1;
+
+	LPC_GPDMACH0->DMACCConfig = (001<<11) + (8 << 6) + 1;
 }
 
 void buildRGB(uint32_t r, uint32_t g,uint32_t b){
 	tabRGB[0] = ~0;
 	int i;
 	for (i = 1; i < 33; ++i) {
-		tabRGB[i] = (r & (1 << i)) >> i;
-		tabRGB[i+32] = (g & (1 << i)) >> i;
-		tabRGB[i+64] = (b & (1 << i)) >> i;
+		tabRGB[i] = ~((r & (1 << i)) >> i);
+		tabRGB[i+32] = ~((g & (1 << i)) >> i);
+		tabRGB[i+64] = ~((b & (1 << i)) >> i);
 	}
 
 }
 
 int main(void) {
 
-    // TODO: insert code here
+    // TODO: Don't choose hardware CS
 
     // Force the counter to be placed into memory
     volatile static int i = 0 ;
     // Enter an infinite loop, just incrementing a counter
     while(1) {
-    	ledSet(255,255,255);
+    	ledSet(255,0,255);
     }
     return 0 ;
 }
